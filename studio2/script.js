a64501dfd324ba1b9535b37e651519e074ceae0e
chao_li <cchli@ucdavis.edu>
2026-04-24 12:44:06 -0700

    update

--- studio2/script.js
+++ studio2/script.js
@@ -2,7 +2,13 @@
     "use strict";
     console.log("running JS");
 
-
+    async function getData(){
+        const bedTimes = await fetch("data/data.json");
+        const data = await bedTimes.json();
+        console.log(data.April13.time, data.April13.status)
+        
+    }
+getData();
 
     
 })()
