37a1af4dd881117974acbec31ccf2472096f5010
chao_li <cchli@ucdavis.edu>
2026-04-26 17:59:12 -0700

    Edit 4/26

--- studio2/script.js
+++ studio2/script.js
@@ -2,13 +2,55 @@
     "use strict";
     console.log("running JS");
 
+   
+// 0 = energized  1 = normal  2 = groggy  3 = very tired
+    const status = [
+        "images/energized.png",
+        "images/normal.png",
+        "images/tired.png",
+        "images/exhausted.png"
+    ]
+    const desc = [
+        'ENERGIZED',
+        'NORMAL', 
+        'TIRED', 
+        'EXHAUSTED'
+    ]
+    const imgContainer = document.querySelector('#statusIMG')
+    const descContainer = document.querySelector('#statusDESC');
+    const labelContainer = document.querySelector("#bed-time");
+
+    const buttons = document.querySelectorAll('#selections button');
+
     async function getData(){
         const bedTimes = await fetch("data/data.json");
         const data = await bedTimes.json();
-        console.log(data.April13.time, data.April13.status)
         
+        buttons.forEach(function(button, index){
+            button.addEventListener('click', function() {
+                const dayKey = 'Day' + (index + 1);
+                const dayData = data[dayKey];
+                const statusIndex = parseInt(dayData.status);
+
+                imgContainer.innerHTML = `<img src="${status[statusIndex]}" alt="${desc[statusIndex]}">`;
+                descContainer.textContent = desc[statusIndex];
+                labelContainer.textContent = "Bed Time: " + dayData.time;
+            })
+        })
+
+  
     }
-getData();
+    getData();
+
+    
+
+
+
+
+
+
+
+
 
     
 })()
